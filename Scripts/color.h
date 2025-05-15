3fe5d1bfd06fcb265e3b627d8eeebe83613e08a5
Nicholas <nicholas@spunow.co.uk>
2025-05-15 20:28:14 +0100

    Generic header fixes and interval class added

--- Scripts/color.h
+++ Scripts/color.h
@@ -1,6 +1,8 @@
 #ifndef COLOR_H
 #define COLOR_H
+
 #include "vec3.h"
+
 using color = vec3;
 
 void write_color(std::ostream& out, const color& pixel_color){
--- Scripts/hittable_list.h
+++ Scripts/hittable_list.h
@@ -3,7 +3,7 @@
 
 #include "hittable.h"
 
-#include <memory>
+
 #include <vector>
 
 using std::make_shared;
@@ -22,13 +22,13 @@ class hittable_list: public hittable{
         objects.push_back(object);
     }
 
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override{
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override{
         hit_record temp_rec;
         bool hit_anything = false;
-        auto closest_so_far = ray_tmax;
+        auto closest_so_far = ray_t.max;
 
         for (const auto& object: objects){
-            if (object->hit(r, ray_tmin,closest_so_far,temp_rec)){ //Check for all objects in list if there is a hit, and save closest hit point
+            if (object->hit(r, interval(ray_t.min,closest_so_far),temp_rec)){ //Check for all objects in list if there is a hit, and save closest hit point
                 hit_anything=true;
                 closest_so_far = temp_rec.t;
                 rec = temp_rec;
