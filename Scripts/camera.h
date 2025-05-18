bf6354c11a70af5f842c79ef9fc607f7b87ee9db
NicholasLambert03 <nicholas@spunow.co.uk>
2025-05-18 23:29:31 +0100

    Reflection cleanup

--- Scripts/camera.h
+++ Scripts/camera.h
@@ -8,6 +8,7 @@ class camera{
     double aspect_ratio = 1.0;
     int image_width = 100;
     int samples_per_pixel = 10;
+    int max_depth = 10; //Maximum number of ray bounces into scene
 
     void render(const hittable& world){
         initialize();
@@ -19,7 +20,7 @@ class camera{
                 color pixel_color(0,0,0);
                 for (int sample=0; sample<samples_per_pixel; sample++){
                     ray r = get_ray(i,j);
-                    pixel_color += ray_color(r,world);
+                    pixel_color += ray_color(r,max_depth,world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
             }
@@ -84,11 +85,16 @@ class camera{
     }
 
 
-    color ray_color(const ray& r, const hittable& world) {
+    color ray_color(const ray& r,int depth, const hittable& world) {
+        //If ray bounces more than max depth, no more light is gathered, return black
+        if (depth <= 0) {
+            return color(0, 0, 0);
+        }
+
         hit_record rec;
-        if (world.hit(r, interval(0, infinity), rec)) {
-            vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+        if (world.hit(r, interval(0.001, infinity), rec)) { // 0.001 to prevent reinteraction with surface
+            vec3 direction = rec.normal + random_unit_vector();
+            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
