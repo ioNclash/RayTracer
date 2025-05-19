7e0dfa321ba7061494bbce1b55e759e6bd9a9786
NicholasLambert03 <nicholas@spunow.co.uk>
2025-05-19 22:01:17 +0100

    material for lambertian diffuse added

--- Scripts/hittable.h
+++ Scripts/hittable.h
@@ -3,16 +3,20 @@
 
 #include "vec3.h"
 
+
+class material;
+
 class hit_record{
     public:
     point3 p;
-    vec3 normal;
+    vec3 normal;\
+    shared_ptr<material> mat;
     double t;
     bool front_face;
 
     void set_face_normal(const ray& r, const vec3& outward_normal) {
         // Sets the hit record normal vector.
-        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+        // the parameter `outward_normal` is assumed to have unit length.
 
         front_face = dot(r.direction(), outward_normal) < 0; //If > 0, 
         normal = front_face ? outward_normal : -outward_normal; //If outward normal is front facing keep positive, else make negative
--- Scripts/material.h
+++ Scripts/material.h
@@ -15,5 +15,26 @@ class material{ //Produce scattered ray or state absorbed, if scattered state ra
     
 };
 
+class lambertian : public material{
+    public:
+    lambertian(const color& albedo): albedo(albedo){}
 
+    bool scatter(
+    const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+        const override{
+            auto scatter_direction = rec.normal + random_unit_vector();
+
+            //catch random vector = -normal degeneration
+            if(scatter_direction.near_zero()){
+                scatter_direction= rec.normal;
+            }
+            scattered = ray(rec.p,scatter_direction);
+            attenuation = albedo;
+            return true;
+
+        }
+
+    private:
+    color albedo;
+};
 #endif
