6a2964dcf275383a6c9ed7121ec98abad22cb6cf
NicholasLambert03 <nicholas@spunow.co.uk>
2025-05-19 22:17:35 +0100

    updated camera to colour based on material

--- Scripts/camera.h
+++ Scripts/camera.h
@@ -93,11 +93,15 @@ class camera{
 
         hit_record rec;
         if (world.hit(r, interval(0.001, infinity), rec)) { // 0.001 to prevent reinteraction with surface
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth-1, world);
+            return color(0,0,0);
         }
 
-        vec3 unit_direction = unit_vector(r.direction());
+        //The sky
+        vec3 unit_direction = unit_vector(r.direction()); 
         auto a = 0.5*(unit_direction.y() + 1.0);
         return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 
