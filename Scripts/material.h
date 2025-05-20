c68beba4d63bd01b60c6eb207a0205b7bb57c228
NicholasLambert03 <nicholas@spunow.co.uk>
2025-05-20 21:48:30 +0100

    Schlick's approximation for reflectance

--- Scripts/material.h
+++ Scripts/material.h
@@ -87,6 +87,14 @@ class dielectric: public material{
 
     private:
     double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index){
+        //Use Schlick's approximation for reflectance
+        auto r0 = (1-refraction_index)/(1+refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*std::pow((1-cosine),5);
+    }
+
 };
 
 #endif
