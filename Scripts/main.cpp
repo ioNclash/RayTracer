36b655611f44f1bbade6126b02f459359c73c474
NicholasLambert03 <nicholas@spunow.co.uk>
2025-05-20 21:24:15 +0100

    Dielectrics added

--- Scripts/main.cpp
+++ Scripts/main.cpp
@@ -11,7 +11,7 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_left   = make_shared<dielectric>(1.50);
     auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
@@ -22,7 +22,7 @@ int main() {
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 3840;
+    cam.image_width  = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50; //Maximum ray reflections
 
