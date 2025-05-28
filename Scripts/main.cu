26d9c9972681f0cea203ce1abe5993b35cc9b93d
Nicholas <nicholas@spunow.co.uk>
2025-05-28 15:38:35 +0100

    Cuda generated ppm

---
+++ Scripts/main.cu
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__)
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line){
+    if(result){
+        std::cerr << "CUDA error =" << static_cast<unsigned int>(result) << " at " <<
+        file << ":" << line << " '" << func << "' \n";
+        cudaDeviceReset();
+        exit(99);
+    }
+}
+
+__global__ void render(float *fb, int max_x, int max_y){
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) return; //Due to block size, prevent rendering outside of image
+    int pixel_index = j*max_x*3 + i*3; //get index in frame buffer
+    fb[pixel_index + 0] = float(i) / max_x;
+    fb[pixel_index + 1] = float(j) / max_y;
+    fb[pixel_index + 2] = 0.2;
+}
+
+
+__host__ int main(){
+    int nx = 1600;  // image width
+    int ny = 900;   // image height
+
+
+    int num_pixels = nx*ny;
+    size_t fb_size = 3*num_pixels*sizeof(float); //frame buffer
+
+    //Allocate framb buffer
+    float *fb;
+    checkCudaErrors(cudaMallocManaged((void **)&fb , fb_size));
+
+
+    //Render buffer
+    int tx = 8;
+    int ty = 8;
+
+    dim3 blocks(nx/tx+1,ny/ty+1);
+    dim3 threads(tx,ty);
+    render<<<blocks, threads>>>(fb, nx,ny);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    //Write image
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            size_t pixel_index = j*3*nx + i*3;
+            float r = fb[pixel_index + 0];
+            float g = fb[pixel_index + 1];
+            float b = fb[pixel_index + 2];
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    checkCudaErrors(cudaFree(fb));
+}
+
+
+
